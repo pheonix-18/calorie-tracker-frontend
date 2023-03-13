b5ff86783de3312856d9e09ab956585a6d1e8ebd
pheonix_18 <sarath.mannam13@knights.ucf.edu>
2023-03-13 15:53:13 -0400

    Modified Login UI using Flex

--- src/Components/login2.jsx
+++ src/Components/login2.jsx
@@ -1,125 +1,154 @@
 // import react
-import React from 'react';
+import React from "react";
 // import browser router and route from react-router-dom
-import { BrowserRouter as Router, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, useNavigate } from "react-router-dom";
 
-
-import wallpaper from '../wallpaper_2.jpeg';
-import logo from '../logo.png';
-import { AuthContext } from '../AuthProvider.jsx';
+import wallpaper from "../wallpaper_2.jpeg";
+import logo from "../logo.png";
+import food_thumbnail from "../food_thumbnail.jpeg";
+import { AuthContext } from "../AuthProvider.jsx";
 // Import cookies from js-cookie
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
-// create a Special Login function component 
+// create a Special Login function component
 export default function Login2() {
-    const {handleLogin} = React.useContext(AuthContext);
-    function handleGoogleSubmit(e) {
-        e.preventDefault();
-        console.log("Google button clicked");
-        window.alert("Google Button Not Implemented Yet");
-    }
-    function handleLoginSubmit(e) {
-        e.preventDefault();
-        // Display the email and password in the console
-        handleLogin(document.getElementById('email').value, document.getElementById('password').value);
-    }
+  const { handleLogin } = React.useContext(AuthContext);
+  function handleGoogleSubmit(e) {
+    e.preventDefault();
+    console.log("Google button clicked");
+    window.alert("Google Button Not Implemented Yet");
+  }
+  function handleLoginSubmit(e) {
+    e.preventDefault();
+    // Display the email and password in the console
+    handleLogin(
+      document.getElementById("email").value,
+      document.getElementById("password").value
+    );
+  }
 
-    
-    return (
-<section className="h-screen w-screen">
-  <div className="px-6 h-fill text-black">
-    <div
-      className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
-    >
+  return (
+    <section className="h-screen w-screen">
       <div
-        className="object-fill grow-0 shrink-1 md:shrink-0 basis-auto w-full xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
+        className="flex h-screen flex-row items-center flex-grow-0 bg-yellow-400  gap-1"
+        style={{
+          backgroundImage: `url(${food_thumbnail})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+        }}
       >
-
-        <img
-            src={wallpaper}
-            className=" py-6 mx-auto h-full "
-            alt="Sample"
-        />
-        
-      </div>
-      <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-        <form onSubmit={handleLoginSubmit}>
-            <img className="h-18 w-32 pt-4 mb-0 pb-4 mx-auto" src={logo} alt="logo" />
-          <div className="flex flex-row items-center justify-center lg:justify-start">
-            <p className="text-lg mb-0 mr-4">Sign in with</p>
-            <button
-              type="button"
-              //send onClick event to handleGoogleSubmit function
-              onClick={handleGoogleSubmit}
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              className="inline-block p-3 bg-white text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-100 hover:shadow-lg focus:bg-gray-200 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/><path d="M1 1h22v22H1z" fill="none"/></svg>
-            </button>
-          </div>
-
-          <div
-            className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
-          >
-            <p className="text-center font-semibold mx-4 mb-0">Or</p>
-          </div>
-
-          
-          <div className="mb-6">
-            <input
-              type="text"
-              className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-pink-600 focus:outline-none"
-              id="email"
-              placeholder="Email address"
-            />
-          </div>
-
-          
-          <div className="mb-6">
-            <input
-              type="password"
-              className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-pink-600 focus:outline-none"
-              id="password"
-              placeholder="Password"
-            />
-          </div>
-
-          <div className="flex justify-between items-center mb-6">
-            <div className="form-group form-check">
-              <input
-                type="checkbox"
-                className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-pink-600 checked:border-pink-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                id="exampleCheck2"
-              />
-              <label className="form-check-label inline-block text-gray-800" htmlFor="exampleCheck2"
-                >Remember me</label
-              >
+        <div className="flex-grow "></div>
+        <div className="flex-grow flex flex-col gap-2 items-center">
+          <form onSubmit={handleLoginSubmit}>
+            <div className="flex flex-row justify-center">
+              <img className="h-18 w-32 mb-2" src={logo} alt="logo" />
             </div>
-            <a href="#!" className="text-gray-100">Forgot password?</a>
-          </div>
-
-          <div className="text-center lg:text-left">
-            <button
-              type="submit"
-              className="inline-block px-7 py-3 bg-pink-400 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-pink-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out"
-            >
-              Login
-            </button>
-            <p className="text-sm font-semibold mt-2 pt-1 mb-0">
-              Don't have an account?
-              <a
-                href="#!"
-                className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-                >Register</a
+            <div className=" flex flex-row items-center justify-start lg:justify-start">
+              <p className=" text-lg mb-0 mr-4">Sign in with</p>
+              <button
+                type="button"
+                //send onClick event to handleGoogleSubmit function
+                onClick={handleGoogleSubmit}
+                data-mdb-ripple="true"
+                data-mdb-ripple-color="light"
+                className="inline-block p-3 bg-white text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-100 hover:shadow-lg focus:bg-gray-200 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
               >
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</section>
-)
-}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  width="24"
+                >
+                  <path
+                    d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+                    fill="#4285F4"
+                  />
+                  <path
+                    d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+                    fill="#34A853"
+                  />
+                  <path
+                    d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+                    fill="#FBBC05"
+                  />
+                  <path
+                    d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+                    fill="#EA4335"
+                  />
+                  <path d="M1 1h22v22H1z" fill="none" />
+                </svg>
+              </button>
+            </div>
 
+            <div className="flex items-center my-3 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+              <p className="text-center font-semibold mx-4 mb-0">Or</p>
+            </div>
+
+            <div className="w-96 mb-4">
+              <input
+                type="text"
+                className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-pink-600 focus:outline-none"
+                id="email"
+                placeholder="Email address"
+              />
+            </div>
+
+            <div className="w-96 mb-2">
+              <input
+                type="password"
+                className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-pink-600 focus:outline-none"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="flex justify-between items-center mb-6">
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-pink-600 checked:border-pink-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                  id="exampleCheck2"
+                />
+                <label
+                  className="form-check-label inline-block text-gray-800"
+                  htmlFor="exampleCheck2"
+                >
+                  Remember me
+                </label>
+              </div>
+              <a href="#!" className="text-gray-100">
+                Forgot password?
+              </a>
+            </div>
+
+            <div className="flex flex-col justify-self-center items-center text-center lg:text-left">
+              <div className="flex flex-row justify-between gap-4">
+                <button
+                  type="submit"
+                  className="inline px-7 py-3 bg-pink-400 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-pink-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Login
+                </button>
+                <button
+                  type="submit"
+                  className="inline px-7 py-3 bg-pink-400 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-pink-700 hover:shadow-lg focus:bg-pink-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Sign up
+                </button>
+              </div>
+              <p className="text-sm font-semibold mt-2 pt-1 mb-0">
+                Don't have an account?
+                <a
+                  href="#!"
+                  className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                >
+                  Register
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
